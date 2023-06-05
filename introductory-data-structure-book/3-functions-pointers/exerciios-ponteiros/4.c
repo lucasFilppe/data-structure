c29d90be12b311c1776994e7f1b72369b20c8090
lucasFilppe <lucasfilipe2323@icloud.com>
2023-06-05 17:22:52 -0300

    ponteiros: 4.c(troca valores)

--- introductory-data-structure-book/3-functions-pointers/exerciios-ponteiros/4.c
+++ introductory-data-structure-book/3-functions-pointers/exerciios-ponteiros/4.c
@@ -2,3 +2,29 @@
 2 variaveis e troque o seu conte ´ udo, ou seja, esta func¸ ´ ao˜ e chamada passando duas ´
 variaveis A e B por exemplo e, ap ´ os a execuc¸ ´ ao da func¸ ˜ ao, A conter ˜ a o valor de B e B ´
 tera o valor de A.*/ 
+
+#include <stdio.h>
+
+void troca(int *a, int *b){
+    
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main()
+{
+    int num1, num2;
+    
+    printf("Digite os dois valores: ");
+    scanf("%d %d", &num1, &num2);
+    
+    troca(&num1, &num2);
+    
+    printf("Valor de a = %d\n", num1);
+    printf("Valor de b = %d\n", num2);
+    
+    
+
+    return 0;
+}
